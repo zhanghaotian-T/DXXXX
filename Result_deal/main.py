09c3bd753254429c7464240fe120257f27cbc8b5
work_computer <893935189@qq.com>
2021-07-14 23:31:54 +0800

    result deal main finishi

--- Result_deal/main.py
+++ Result_deal/main.py
@@ -1,4 +1,6 @@
 import re
+import pandas as pd
+import os
 
 
 class ResultDeal(object):
@@ -9,102 +11,76 @@ class ResultDeal(object):
         self.channel = None
         self.aclr_l1_pre = 0
         self.aclr_u1_pre = 0
+        self.sn = None
 
     def run(self):
-        pass
+        file_path_list = self.file_ergodic()
+        for file_path in file_path_list:
+            self.file_open(file_path)
+        print(file_path_list)
+        self.save_report_log(self.report)
 
-    def file_open(self):
-        with open('Result_log/ResultsLog.txt') as f:
+    def file_open(self, file_path):
+        print(file_path)
+        self.sn = re.findall(r'\\.*.txt', file_path)[0][1: -4]
+        with open(file_path, encoding='UTF-8', errors='replace') as f:
             for index, line in enumerate(f):
+                print(line)
                 if 'Test Model' in line:
                     self.test_model = re.findall(r'LTE_ETM[0-9]_\w+', line)[0]
                 if "Channel#" in line:
                     self.channel = re.findall(r'[0-9]+', line)[0]
                 if ("ACLR_EUTRA_L1" in line) and (index - self.aclr_l1_pre != 8):
                     aclr_l1 = re.findall(r'[0-9]+.[0-9]+', line)[0]
-                    self.report.append(self.ant + '_' + self.test_model + '_' + self.channel + '_' + aclr_l1)
+                    self.report.append(
+                        self.sn + ';' + self.ant + ';' + self.test_model + ';' + self.channel + ';' + 'ACLR_L1;' + aclr_l1)
                     self.aclr_l1_pre = index
                 if ('ACLR_EUTRA_U1' in line) and (index - self.aclr_u1_pre != 8):
                     aclr_u1 = re.findall(r'[0-9]+.[0-9]+', line)[0]
-                    self.report.append(self.ant + '_' + self.test_model + '_' + self.channel + '_' + aclr_u1)
+                    self.report.append(
+                        self.sn + ';' + self.ant + ';' + self.test_model + ';' + self.channel + ';' + 'ACLR_U1;' + aclr_u1)
                     self.aclr_u1_pre = index
+                if ('Tx Mean Power Average' in line) and (self.test_model == 'LTE_ETM1_1'):
+                    tx_mean_power = re.findall(r'[0-9]+.[0-9]+', line)[0]
+                    self.report.append(
+                        self.sn + ';' + self.ant + ';' + self.test_model + ';' + self.channel + ';' + 'TX_MEAN_POWER;' + tx_mean_power)
+                if 'EVM Average' in line:
+                    evm = re.findall(r'[0-9]+.[0-9]+', line)[0]
+                    self.report.append(
+                        self.sn + ';' + self.ant + ';' + self.test_model + ';' + self.channel + ';' + 'EVM;' + evm)
+                if 'CCDF Power Level at 0.1%' in line:
+                    ccdf = re.findall(r'[0-9]+.[0-9]+', line)[1]
+                    self.report.append(
+                        self.sn + ';' + self.ant + ';' + self.test_model + ';' + self.channel + ';' + 'CCDF 0.1%;' + ccdf)
+                if 'END OF LTE Tx MODULATION RESULTS' in line:
+                    self.ant = 'ANT_2'
+        self.ant = "ANT_1"
+        f.close()
+        return self.report
 
+    def save_report_log(self, result_list: list):
+        print(result_list)
+        result_list_dataframe = [x.split(';') for x in result_list]
+        final_result = pd.DataFrame(result_list_dataframe,
+                                    columns=['SN', 'ANT', 'Test_Model', 'Frequency', 'Test_Case', 'Result'])
+        if not os.path.exists(r'Report'):
+            os.makedirs(r'Report')
+            final_result.to_csv(r'Report/Result_Report.csv', encoding='utf-8')
+            print(final_result)
+        else:
+            final_result.to_csv(r'\Report\Result_Report.csv')
+            print(final_result)
 
-
-
-    def key_word_fil(self):
-        pass
-
-    def line_key_word(self, line):
-        try:
-            channel = re.findall(r'Channel#	\s*[0-9]*', line)
-            alcr_l1 = re.findall(r'ACLR_EUTRA_L1:\s*[0-9]*.[0-9]*', line)
-            aclr_ui = re.findall(r'ACLR_EUTRA_U1:\s*[0-9]*.[0-9]*', line)
-            tx_mean_power = re.findall(r'Tx Mean Power Average\S*\s*[0-9]*.[0-9]*', line)
-            evm = re.findall(r'')
-            evm_average = 0
-            ccdf = 0
-
-        except Exception as e:
-            return None
-
-# def file_open(self):
-    #     with open('Result_log/ResultsLog.txt') as f:
-    #         self.report["ResultsLog"] = {}
-    #         for index, line in enumerate(f):
-    #             print(index)
-    #             if 'Test Model' in line:
-    #                 test_model = re.findall(r'LTE_ETM[0-9]_\w+', line)[0]
-    #                 self.report['ResultsLog'][test_model] = self.test_model
-    #                 self.aclr_l1 = list()
-    #                 self.aclr_ui = list()
-    #                 self.tx_mean_power = list()
-    #                 self.evm = list()
-    #                 self.ccdf = list()
-    #             if "Channel#" in line:
-    #                 channel = re.findall(r'[0-9]+', line)[0]
-    #                 self.report['ResultsLog'][test_model][channel] = self.channel
-    #                 self.aclr_l1 = list()
-    #                 self.aclr_ui = list()
-    #                 self.tx_mean_power = list()
-    #                 self.evm = list()
-    #                 self.ccdf = list()
-    #             if "ACLR_EUTRA_L1" in line:
-    #                 aclr_l1 = re.findall(r'[0-9]+.[0-9]+', line)[0]
-    #                 self.aclr_l1.append(aclr_l1)
-    #                 if (index - self.aclr_l1_index_pre) == 8:
-    #                     aclr_l1_average = (float(self.aclr_l1[-1]) + float(self.aclr_l1[-2])) / 2
-    #                     del self.aclr_l1[-2:]
-    #                     self.aclr_l1.append(aclr_l1_average)
-    #                 self.report['ResultsLog'][test_model][channel]['ACLR_EUTRA_L1'] = self.aclr_l1
-    #                 self.aclr_l1_index_pre = index
-    #             if 'ACLR_EUTRA_U1' in line:
-    #                 aclr_u1 = re.findall(r'[0-9]+.[0-9]+', line)[0]
-    #                 self.aclr_ui.append(aclr_u1)
-    #                 if (index - self.aclr_ui_index_pre) == 8:
-    #                     aclr_l1_average = (float(self.aclr_ui[-1]) + float(self.aclr_ui[-2])) / 2
-    #                     del self.aclr_ui[-2:]
-    #                     self.aclr_ui.append(aclr_l1_average)
-    #                 self.report['ResultsLog'][test_model][channel]['ACLR_EUTRA_U1'] = self.aclr_ui
-    #                 self.aclr_ui_index_pre = index
-    #             if 'Tx Mean Power Average' in line:
-    #                 if test_model == 'ETM1_1':
-    #                     tx_reference_power = re.findall(r'[0-9]+.[0-9]+', line)
-    #                     self.tx_mean_power.append(tx_reference_power)
-    #                     self.report['ResultsLog'][test_model][channel]['TX Mean Power Average'] = self.tx_mean_power
-    #             if 'EVM Average' in line:
-    #                 evm = re.findall(r'[0-9]+.[0-9]+', line)
-    #                 self.evm.append(evm)
-    #                 self.report['ResultsLog'][test_model][channel]['EVM Average'] = self.tx_mean_power
-    #             if 'CCDF Power Level at 0.1%' in line:
-    #                 ccdf = re.findall(r'[0-9]+.[0-9]+', line)
-    #                 self.ccdf.append(ccdf)
-    #                 self.report['ResultsLog'][test_model][channel]['CCDF Power Level at 0.1%'] = self.ccdf
-    #     return self.report
+    def file_ergodic(self):
+        file_path_main = 'Result_log'
+        path_list = list()
+        for i in os.listdir(file_path_main):
+            file_path = os.path.join('Result_log', i)
+            path_list.append(file_path)
+        return path_list
 
 
 if __name__ == '__main__':
     a = ResultDeal()
-    b = a.file_open()
-    pass
-
+    a.run()
+    input()
