f2919defec378375c19b99d60165d1bdf87b0f74
work_computer <893935189@qq.com>
2021-07-27 22:51:31 +0800

    16进制算法

--- delay_caculate/main.py
+++ delay_caculate/main.py
@@ -18,36 +18,42 @@ class DelayCaculate(QMainWindow):
         self.ui.lineEdit_2.returnPressed.connect(self.caculate_time)
 
     def promt_message(self):
-        self.ui.lineEdit.setText('0x507')
+        self.ui.lineEdit.setText('0x')
         self.ui.lineEdit_2.setText(chr(0x0075) + 's')
+        self.ui.lineEdit_3.setText(chr(0x0075) + 's')
+        self.ui.comboBox.addItems(['Forward', 'Delay'])
         # self.ui.lineEdit.setReadOnly(True)
         # self.ui.lineEdit.setStyleSheet("color: gray")
 
     def primary_caculate_time(self):
-        primary_time_source = self.ui.lineEdit.text()
-        primary_time_hex = primary_time_source[2:]
-        primary_time_int = int(primary_time_hex, 16)
-        self.ui.comboBox.addItems(['Forward', 'Delay'])
-        if primary_time_int >= 6140:
-            self.ui.comboBox.setCurrentText('Forward')
-            primary_time = round(((65536 - primary_time_int) / 122.88), 2)
-            self.ui.lineEdit_3.setText(str(primary_time) + chr(0x0075) + 's')
-        else:
-            self.ui.comboBox.setCurrentText('Delay')
-            primary_time = round((primary_time_int / 122.88), 2)
-            self.ui.lineEdit_3.setText(str(primary_time) + chr(0x0075) + 's')
+        try:
+            primary_time_source = self.ui.lineEdit.text()
+            primary_time_hex = primary_time_source[2:]
+            primary_time_int = int(primary_time_hex, 16)
+            self.ui.comboBox.addItems(['Forward', 'Delay'])
+            if primary_time_int >= 6140:
+                self.ui.comboBox.setCurrentText('Forward')
+                primary_time = round(((65536 - primary_time_int) / 122.88), 2)
+                self.ui.lineEdit_3.setText(str(primary_time) + chr(0x0075) + 's')
+            else:
+                self.ui.comboBox.setCurrentText('Delay')
+                primary_time = round((primary_time_int / 122.88), 2)
+                self.ui.lineEdit_3.setText(str(primary_time) + chr(0x0075) + 's')
+        except Exception as e:
+            print(e)
 
     def caculate_time(self):
-        primary_date_hex = self.ui.lineEdit.text()[2:]
+        self.primary_caculate_time()
+        primary_date_hex = self.ui.lineEdit.text()
         if self.ui.checkBox.checkState():
-            delta_time_int = self.ui.lineEdit_2.text()[:-2]
-            date_time_hex = float(delta_time_int) * 122.88
-            adv_result = hex(int(primary_date_hex, 16) - int(date_time_hex, 16))
+            delta_time_str = self.ui.lineEdit_2.text()[:-2]
+            date_time_int = float(delta_time_str) * 122.88
+            adv_result = hex(int(int(primary_date_hex, 16) + date_time_int))
             self.ui.lineEdit_4.setText(adv_result)
         if self.ui.checkBox_2.checkState():
-            delta_time_int = self.ui.lineEdit_2.text()[:-2]
-            date_time_hex = float(delta_time_int) * 122.88
-            delay_result = hex(primary_date_hex + date_time_hex)
+            delta_time_str = self.ui.lineEdit_2.text()[:-2]
+            date_time_int = float(delta_time_str) * 122.88
+            delay_result = hex(int(int(primary_date_hex, 16) - date_time_int))
             self.ui.lineEdit_4.setText(delay_result)
 
 
