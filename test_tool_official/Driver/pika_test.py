c4e90f66ef4cbed630a323b734859d937d9167be
zhanghaotian-T <89393589@qq.com>
2021-09-22 17:50:09 +0800

    add pika_test.py

---
+++ test_tool_official/Driver/pika_test.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:Haotian
+@file: pika_test.py
+@time: 2021/09/20
+"""
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='hello world')
+
+print('[x] sent hello world')
+
+connection.close()
+
+if __name__ == '__main__':
+    print('Python')
