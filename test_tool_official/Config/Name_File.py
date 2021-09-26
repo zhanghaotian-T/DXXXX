03aa73943f8db5f3a08b22145a5c1c69feec053f
work_computer <893935189@qq.com>
2021-09-26 22:57:00 +0800

    add pika

---
+++ test_tool_official/Config/Name_File.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :Name_File.py
+# @Time      :2021/9/25 19:19
+# @Author    :Haotian
+
+
+RRU_TYPE = 'RRU_Type'
+TYPE = 'Type'
+IP = 'IP'
+
+if __name__ == "__main__":
+    run_code = 0
--- test_tool_official/Thread/RRU_Thread.py
+++ test_tool_official/Thread/RRU_Thread.py
@@ -4,6 +4,39 @@
 # @Time      :2021/9/23 23:36
 # @Author    :Haotian
 
+import threading
+import pika, sys, os
+from Config.Name_File import *
+import yaml
+from loguru import logger
+from Driver.bci import Bci
+
+
+class RruThread(threading.Thread):
+    def __init__(self, threadname):
+        threading.Thread.__init__(self, name='Threading' + threadname)
+        self.rru = None
+
+    def run(self):
+        self.rru_type_config()
+        self.rru_queue_deal()
+        pass
+
+    def rru_type_config(self):
+        if not self.rru:
+            config_file = open(r'../Config/Type_Config.yaml')
+            rru_type = yaml.load(config_file, Loader=yaml.FullLoader)
+            if rru_type[RRU_TYPE][TYPE] == 'O-RAN':
+                pass
+            elif rru_type[RRU_TYPE][TYPE] == '玄铁':
+                self.rru = Bci(host=rru_type[RRU_TYPE][IP], user='dg', password='passw0rd')
+            else:
+                logger.warning('Please Input right RRU Type')
+
+    def rru_queue_deal(self):
+
+        pass
+
 
 if __name__ == "__main__":
-    run_code = 0
+    pass
