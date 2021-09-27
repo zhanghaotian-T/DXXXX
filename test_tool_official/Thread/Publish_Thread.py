3d3ba73e88a93f6360d26ef02faf62a482541d2b
work_computer <893935189@qq.com>
2021-09-27 23:00:53 +0800

    change kombu

--- test_tool_official/Thread/Publish_Thread.py
+++ test_tool_official/Thread/Publish_Thread.py
@@ -4,26 +4,36 @@
 # @Time      :2021/9/23 23:22
 # @Author    :Haotian
 import json
-import pika
+from kombu import Connection, Exchange, Producer, Queue
 
 
 class PublishThread(object):
-    def __init__(self):
+    def __init__(self, amqp_url='amqp://localhost:5672/', exchange_name='example-exchange', route_key='BOB', queue_name='example-queue'):
+        self.amqp_url = amqp_url
+        self.exchange_name = exchange_name
+        self.route_key = route_key
+        self.queue_name = queue_name
         self.connection = None
-        self.channel = None
-        self._connect()
+        self.puducer = None
+        self.queue = None
 
     def _connect(self):
-        if not self.connection:
-            self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-            self.channel = self.connection.channel()
+        if not self.puducer:
+            self.connection = Connection(self.amqp_url)
+            channel = self.connection.channel()
+            exchange = Exchange(self.exchange_name, type='direct')
+            self.puducer = Producer(exchange=exchange, channel=channel, routing_key=self.route_key)
+            self.queue = Queue(name=self.queue_name, exchange=exchange, routing_key=self.route_key)
+            self.queue.maybe_bind(self.connection)
+            self.queue.declare()
 
-    def sender(self, queue_name, sende_message=dict):
-        self.channel.basic_publish(
-            exchange='',
-            routing_key=queue_name,
-            body=json.dumps(sende_message)
-        )
+    def send_message(self):
+        pass
+
+
+
+
+
 
 
 if __name__ == "__main__":
