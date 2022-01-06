e9b824404030858a65a6111bae03ba7de654b270
zhanghaotian-T <89393589@qq.com>
2022-01-06 19:09:18 +0800

    增加sFTP协议传输文件

--- BBU_Config/CETC_BBU_Conf_edit.py
+++ BBU_Config/CETC_BBU_Conf_edit.py
@@ -25,45 +25,59 @@ class CETCbbuEdite(BbuconfdbReconfig):
         self.xml_save()
 
     def complete_frequency_edit_dict(self):
-
-        if self.edit_dict['band'] == 41:
+        if self.edit_dict['services:band'] == 41:
             ssb_offset = 6
             abs_ssb_freq = 504990
             abs_point_a = 503172
             pdcch_config_sib = 160
-        elif self.edit_dict['band'] == 78:
+            msg_lenghth = 400
+        elif self.edit_dict['services:band'] == 78:
             ssb_offset = 4
             abs_ssb_freq = 630720
             abs_point_a = 630164
             pdcch_config_sib = 64
-        elif self.edit_dict['band'] == 79:
+            msg_lenghth = 400
+        elif self.edit_dict['services:band'] == 79:
             ssb_offset = 6
             abs_ssb_freq = 721824
             abs_point_a = 720060
             pdcch_config_sib = 64
+            msg_lenghth = 150
         else:
             ssb_offset = 6
             abs_ssb_freq = 504990
             abs_point_a = 503172
-            pdcch_config_sib = 64
+            pdcch_config_sib = 160
+            msg_lenghth = 400
             logger.info('The service: {} is error'.format(self.edit_dict['band']))
-        key_message_list = ['ssb-frequency', 'ref-frequency-csi-rs', 'pdcch-config-sib1', 'ssb-subcarrier-offset', 'absolute-frequency-point-a']
+        key_message_list = ['services:ssb-frequency',
+                            'services:pdcch-config-sib1',
+                            'services:ssb-subcarrier-offset',
+                            'services:absolute-frequency-ssb',
+                            'services:absolute-frequency-point-a',
+                            'services:ref-freq-csi-rs',
+                            'services:bo-report-msg-length',
+                            'services:freq-band-indicator']
         for message in key_message_list:
             if message in self.edit_dict.keys():
                 continue
             else:
-                if message == 'ssb-frequency':
-                    self.edit_dict['ssb-frequency'] = abs_ssb_freq
-                elif message == 'ref-frequency-csi-rs':
-                    self.edit_dict['ref-frequency-csi-rs'] = abs_ssb_freq
-                elif message == 'pdcch-config-sib1':
-                    self.edit_dict['pdcch-config-sib1'] = pdcch_config_sib
-                elif message == 'ssb-subcarrier-offset':
-                    self.edit_dict['ssb-subcarrier-offset'] = ssb_offset
-                elif message == 'absolute-frequency-point-a':
-                    self.edit_dict['absolute-frequency-point-a'] = abs_point_a
-                elif message == 'absolute-frequency-ssb':
-                    self.edit_dict['absolute-frequency-ssb'] = abs_ssb_freq
+                if message == 'services:ssb-frequency':
+                    self.edit_dict['services:ssb-frequency'] = abs_ssb_freq
+                elif message == 'services:absolute-frequency-ssb':
+                    self.edit_dict['services:absolute-frequency-ssb'] = abs_ssb_freq
+                elif message == 'services:ref-freq-csi-rs':
+                    self.edit_dict['services:ref-freq-csi-rs'] = abs_ssb_freq
+                elif message == 'services:pdcch-config-sib1':
+                    self.edit_dict['services:pdcch-config-sib1'] = pdcch_config_sib
+                elif message == 'services:ssb-subcarrier-offset':
+                    self.edit_dict['services:ssb-subcarrier-offset'] = ssb_offset
+                elif message == 'services:absolute-frequency-point-a':
+                    self.edit_dict['services:absolute-frequency-point-a'] = abs_point_a
+                elif message == 'services:bo-report-msg-length':
+                    self.edit_dict['services:bo-report-msg-length'] = msg_lenghth
+                elif message == 'services:freq-band-indicator':
+                    self.edit_dict['services:freq-band-indicator'] = self.edit_dict['services:band']
                 else:
                     logger.info('The key of config yaml is error, please check the config file')
 
@@ -78,9 +92,7 @@ class CETCbbuEdite(BbuconfdbReconfig):
             self.conf_replace(key_world, str(self.edit_dict[key_world]))
 
     def xml_save(self):
-        with open(self.file_path, 'wb') as file:
-            self.soup.encode(formatter=None)
-            file.write(self.soup.prettify('utf-8'))
+        self.save_xml()
 
 
 if __name__ == '__main__':
--- BBU_Config/Driver/xml_edit.py
+++ BBU_Config/Driver/xml_edit.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @FileName  :xml_edit_1.py
+# @FileName  :xml_edit.py
 # @Time      :2021/12/11 21:02
 # @Author    :Haotian
-import os
-import re
-try:
-    import xml.etree.cElementTree as ET
-except ImportError:
-    import xml.etree.ElementTree as ET
+
 import yaml
-from loguru import logger
+import xml.dom.minidom
 
 
 class BbuconfdbReconfig(object):
@@ -18,39 +13,32 @@ class BbuconfdbReconfig(object):
     def __init__(self, file_path):
         self.config_message = None
         self.conf_path = file_path
-        self.tree = None
+        self.dom = None
         self.root = None
 
     def run(self):
         self.conf_get()
-        self.conf_replace('band', '41')
+        self.conf_replace('services:band', '41')
         self.save_xml()
 
     def message_get(self):
         self.config_message = yaml.load(open('../BBUconfig.yaml', 'r', encoding='utf-8'), Loader=yaml.FullLoader)
 
     def conf_get(self):
-        if not self.tree:
-            try:
-                self.tree = ET.parse(self.conf_path)
-                self.root = self.tree.getroot()
-            except Exception as e:
-               logger.info('Please Input right file')
+        if not self.dom:
+            self.dom = xml.dom.minidom.parse(self.conf_path)
+            self.root = self.dom.documentElement
 
     def conf_replace(self, key_name, replaced_string):
-        for element in self.root.iter():
-            element_tag = element.tag.split('}')[1]
-            if key_name == element_tag:
-                element.text = replaced_string
-                logger.info('The text {} change to {}'.format(element.text, replaced_string))
-            else:
-                logger.info('there is no message need replace')
+        key_node_list = self.root.getElementsByTagName(key_name)
+        for node in key_node_list:
+            node.childNodes[0].data = str(replaced_string)
 
     def save_xml(self):
-        self.tree.write(self.conf_path, 'utf-8')
+        with open(self.conf_path, 'w', encoding='utf-8') as file:
+            self.dom.writexml(file, encoding='utf-8')
 
 
 if __name__ == "__main__":
     BBU_reconfig = BbuconfdbReconfig('../BBUconfdb/confdb.xml')
     BBU_reconfig.run()
-
